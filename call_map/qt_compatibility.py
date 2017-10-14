a927e44ddc47e4750a1fdf0ddbdd0cee662d5d3a
Andrew Lee <ajylee@users.noreply.github.com>
2017-10-14 11:23:21 -0700

    Change to PySide2

--- call_map/qt_compatibility.py
+++ call_map/qt_compatibility.py
@@ -4,7 +4,7 @@ The qt_compatibility module is to make it easy to switch between different Qt/Py
 '''
 
 
-from PyQt5 import QtCore, QtGui, QtWidgets, Qt
+from PySide2 import QtCore, QtGui, QtWidgets, Qt
 
 # The following monkey patching already occurs when importing
 # qtconsole.pygments_highlighter, however that should not be relied upon.
